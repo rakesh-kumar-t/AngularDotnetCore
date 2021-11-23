cb3b98eae70e07a6ec5dec01ea88f71fc3d66ead
Rakesh <rakeshkumart2760@gmail.com>
2021-11-23 22:29:08 +0530

    insert method implemented

--- WebAPI/WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/WebAPI/Controllers/DepartmentController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Data;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -43,6 +44,29 @@ namespace WebAPI.Controllers
 
             return new JsonResult(table);
 
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            string query = @"
+                            insert into dbo.Department values('"+dep.DepartmentName+@"')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
         }
     }
 }
