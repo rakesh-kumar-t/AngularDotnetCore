980dd306b0ae69e51ca9848997644f06996f058f
Rakesh <rakeshkumart2760@gmail.com>
2021-11-23 18:16:42 +0530

    get method for getting data from table

---
+++ WebAPI/WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select DepartmentId,DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand=new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+
+        }
+    }
+}
---
+++ dbCommands/AngularDotnet Learning.sql
@@ -0,0 +1,30 @@
+create database EmployeeDB
+
+use EmployeeDB
+
+create table dbo.Department(
+DepartmentId int identity(1,1),
+DepartmentName varchar(500)
+);
+
+select * from dbo.Department;
+
+insert into dbo.Department values('Support');
+
+
+insert into dbo.Department values('IT');
+
+create table dbo.Employee(
+EmployeeId int identity(1,1),
+EmployeeName varchar(500),
+Department varchar(500),
+DateOfJoining date,
+PhotoFileName varchar(500)
+)
+
+insert into dbo.Employee values(
+'Sam','IT','2020-06-02','sam.png'
+)
+
+select * from dbo.Employee;
+
